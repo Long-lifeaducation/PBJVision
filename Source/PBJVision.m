260f00438e7be6683c143db38984aa589a0a87af
marktgodfrey <mark.godfrey@smule.com>
2014-10-01 16:04:50 -0700

    new iphones (6,6+) use color filters

--- Source/PBJVision.m
+++ Source/PBJVision.m
@@ -913,7 +913,7 @@ typedef void (^PBJVisionBlock)();
     
     self.filteringEnabled = NO;
     NSString *currentHardware = [PBJVision hardwareString];
-    for (NSString *device in @[@"iPhone6", @"iPad3"]) {
+    for (NSString *device in @[@"iPhone6", @"iPad3", @"iPhone7"]) {
         if ([currentHardware hasPrefix:device]) {
             self.filteringEnabled = YES;
             break;
